571f0d4c576f232ee46a9bf15196f4cfaa48e8bf
Jon <jmcharter@gmail.com>
2020-07-11 11:18:07 +0100

    Add project files.

---
+++ Engine/Models/StringInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    class StringInput : INotifyPropertyChanged
+    {
+        private string _getUserInput;
+
+        public string UserInput
+        {
+            get { return _getUserInput; }
+            set
+            {
+                UserInput = value;
+                OnPropertyChanged("UserInput");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Engine/ViewModels/ConverterVM.cs
@@ -0,0 +1,21 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    class ConverterVM
+    {
+        public StringInput Reverser { get; set; }
+
+        public ConverterVM()
+        {
+            Reverser = new StringInput();
+            Reverser.UserInput = "Type here...";
+
+        }
+    }
+}
