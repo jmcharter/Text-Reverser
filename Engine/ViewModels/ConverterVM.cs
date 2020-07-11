5b414af9dcf8cddb75faa706f535cf002ffbb837
Jon <jmcharter@gmail.com>
2020-07-11 19:45:16 +0100

    Removed placed holder text. Created ReverseString() method.

--- Engine/ViewModels/ConverterVM.cs
+++ Engine/ViewModels/ConverterVM.cs
@@ -14,9 +14,25 @@ namespace Engine.ViewModels
         public ConverterVM()
         {
             Reverser = new StringInput();
-            Reverser.UserInput = "Type here...";
+            Reverser.UserInput = "";
             Reverser.UserOutput = "";
 
+        }
+
+        public void ReverseString()
+        {
+            List<char> input_chars = new List<char>();
+
+            foreach (char c in Reverser.UserInput )
+            {
+                input_chars.Add(c);
+            }
+
+            input_chars.Reverse();
+
+            string reversed_string = string.Join("", input_chars.ToArray());
+
+            Reverser.UserOutput = reversed_string;
         }
     }
 }
